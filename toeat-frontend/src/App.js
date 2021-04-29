8b6528abda227e3f095b3553a4e40822e148d477
Laura <lauraporto596@gmail.com>
2021-04-29 21:21:14 +0200

    login modal

--- toeat-frontend/src/App.js
+++ toeat-frontend/src/App.js
@@ -6,7 +6,6 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 //Rutas
 import Home from './containers/Home/home';
 import Profile from './containers/Profile/profile';
-import Login from './containers/Login/login';
 import Register from './containers/Register/register';
 
 function App() {
@@ -16,7 +15,6 @@ function App() {
         <Switch>
           <Route path='/' exact component={Home}/>
           <Route path='/profile' exact component={Profile}/>
-          <Route path='/login' exact component={Login}/>
           <Route path='/register' exact component={Register}/> 
         </Switch>
       </BrowserRouter>
