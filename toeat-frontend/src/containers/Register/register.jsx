8796f6ba3ff3b298af7eabb2517b91f5b771dcab
Laura <lauraporto596@gmail.com>
2021-04-29 20:03:43 +0200

    reacstrap added

--- toeat-frontend/src/containers/Register/register.jsx
+++ toeat-frontend/src/containers/Register/register.jsx
@@ -1,19 +1,98 @@
-import React from 'react'
-
+import React, {useState} from 'react'
+import axios from 'axios';
 import cooking1 from '../../assets/cooking1.jpg';
 
+import {validateField, validateFields} from '../../uti';
+
+import { Button, FormGroup, Input, Label, FormFeedback } from 'reactstrap';
+
 const Register = () => {
+
+    //Validación de errores en el formulario
+    const [validationResult, setValidationResult] = useState({
+        validated: false,
+        name: null
+    });
+
+    //Hooks con los datos del usuario
+    const [dataRegister, setRegister] = useState({
+        name: '',
+        surname: '',
+        phone: '',
+        country: '',
+        email: '',
+        password: ''
+    });
+
+
+    //Handlers
+    const handleState = (event) => {
+
+        //
+        setValidationResult({
+            //
+            ...validationResult,
+            //
+            [event.target.name]: validateField(event.target.name, event.target.value)
+        });
+
+        let data = { ...dataRegister, [event.target.name]: event.target.value };
+        setRegister(data)
+        console.log('update data', dataRegister);
+    }
+
+
+    const sendData = async () => {
+        let validationResult = validateFields(dataRegister);
+
+        //Setea el estado de validación
+        setValidationResult({
+            ...validationResult,
+            validated: true
+        });
+
+        let result = await axios.post(`http://localhost:5000/user/register`, dataRegister);
+        console.log(result, 'esto es result');
+    }
+
     return (
         <div className='main-container'>
             <div className='register-container'>
                 <div className='img-container'><img src={cooking1} style={{maxWidth: '100%', width: 'auto', height: 'auto'}}></img></div>
                 <div className='form-container'>
-                    <form action='login.jsx'>
-
-                    </form>
+                    <FormGroup>
+                        <Label for='name'>Name:</Label>
+                        <Input type='text' id='name' name='name' onChange={handleState} valid={validationResult.validated && !validationResult.name} invalid={validationResult.validated && validationResult.name} />
+                        <FormFeedback>{validationResult.name}</FormFeedback>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for='surname1'>Surname:</Label>
+                        <Input type='text' id='surname1' name='surname1' onChange={handleState} valid={validationResult.validated && !validationResult.surname1} invalid={validationResult.validated && validationResult.surname1} />
+                        <FormFeedback>{validationResult.surname1}</FormFeedback>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for='phone'>Teléfono : </Label>
+                        <Input type='number' id='phone' name='phone' onChange={handleState} valid={validationResult.validated && !validationResult.phone} invalid={validationResult.validated && validationResult.phone} />
+                        <FormFeedback>{validationResult.phone}</FormFeedback>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for='surname2'>Country:</Label>
+                        <Input type='text' id='surname2' name='surname2' onChange={handleState} valid={validationResult.validated && !validationResult.surname2} invalid={validationResult.validated && validationResult.surname2} />
+                        <FormFeedback>{validationResult.surname2}</FormFeedback>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label form='email'>Email:</Label>
+                        <Input type='text' id='email' name='email' onChange={handleState} valid={validationResult.validated && !validationResult.email} invalid={validationResult.validated && validationResult.email} />
+                        <FormFeedback>{validationResult.email}</FormFeedback>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label form='password'>Password:</Label>
+                        <Input type='password' id='password' name='password' onChange={handleState} valid={validationResult.validated && !validationResult.password} invalid={validationResult.validated && validationResult.password} />
+                        <FormFeedback>{validationResult.password}</FormFeedback>
+                    </FormGroup>
+                    <Button color='primary' onClick={sendData}>Registrarse</Button>
                 </div>
             </div>
-            
         </div>
     )
 }
