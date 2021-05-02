139662e14601f5074b47adc61e745a8954433740
Laura <lauraporto596@gmail.com>
2021-05-02 18:50:27 +0200

    adding search component to home view

--- toeat-frontend/src/containers/Home/home.jsx
+++ toeat-frontend/src/containers/Home/home.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 import Header from '../../components/Header/header';
 import spices from '../../assets/spices.jpg';
-
+import spaghetti from '../../assets/spaghetti.jpg';
+import fruits from '../../assets/fruits.png';
+import Search from '../../components/Search/search';
 
 const Home = () => {
 
@@ -18,10 +20,18 @@ const Home = () => {
 
                 <div className='cover-page'>
                     <div className="cover-page-img">
-                        <img src={spices} style={{maxWidth: '100%', width: 'auto', height: 'auto',  backgroundsize: 'cover'}}></img>
+                        <img src={spices} style={{maxWidth: '100%', width: 'auto', height: 'auto',  backgroundsize: 'cover', backgroundPosition: 'top center'}}></img>
                     </div>
 
-                    <div className="your-ingredient"></div>
+                    <div className="your-ingredient">
+                        <div className="sub-text">
+                            <h2>¿Quieres probar lo que tus alimentos te ofrecen?</h2>
+                            <Search/>
+                        </div>
+                        <div className="sub-img">
+                            <img src={fruits} style={{maxWidth: '100%', width: 'auto', height: '30em'}}></img>
+                        </div>
+                    </div>
                 </div>
 
             </div>
