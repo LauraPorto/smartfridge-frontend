7d07327888c9e720cb6dc125b0a11d08889b959a
Laura <lauraporto596@gmail.com>
2021-05-04 22:36:11 +0200

    adding design in some components (explore, about)

--- smartfridge-frontend/src/containers/Explore/explore.jsx
+++ smartfridge-frontend/src/containers/Explore/explore.jsx
@@ -3,16 +3,18 @@ import Header from '../../components/Header/header';
 
 import {connect} from 'react-redux';
 import Search from '../../components/Search/search';
-
+import fruits from '../../assets/fruits.png';
 
 const Explore = () => {
     return (
-        <div>
+        <div className="explore-main-container">
             <div className="header-div"> <Header/> </div>
             <div className="searchbox-container">
                 <Search/>
             </div>
-
+            <div className="explore-body">
+                <img src={fruits} style={{maxWidth: '100%', width: 'auto', height: '35em'}}></img>
+            </div>
         </div>
 
     )
