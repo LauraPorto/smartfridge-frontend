f2d9aadd56c643f2eed61e8eb343308b62180b8b
Laura <lauraporto596@gmail.com>
2021-05-05 13:14:18 +0200

    redux in header

--- smartfridge-frontend/src/components/Header/header.jsx
+++ smartfridge-frontend/src/components/Header/header.jsx
@@ -2,14 +2,31 @@ import React from 'react'
 
 import Login from '../Login/login';
 
-const Header = () => {
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDoorOpen } from '@fortawesome/free-solid-svg-icons';
+
+import {connect} from 'react-redux';
+import {LOGOUT} from '../../redux/types/userType';
+
+const Header = (props) => {
 
 
     
+    const logOut = () => {
+        setTimeout(()=> {
+            props.dispatch({ type: LOGOUT, payload : {}});
+        },500);
+    }
 
     return (
         <div className="header-container">
+            
             <ul className="nav justify-content-center">
+
+            {
+                !props.user
+                ?
+                <>
                 <div className="nav-item">
                     <a className="nav-link" href="/about">About Us</a>
                 </div>
@@ -19,15 +36,44 @@ const Header = () => {
                 <div className="nav-item">
                     <a className="nav-link" href="/register">Register</a>
                 </div>
-                <div className="nav-item">
+                {/* <div className="nav-item">
                     <a className="nav-link" href="">Log Out</a>
+                </div> */}
+                <div className='btn-login'>
+                    <Login/>
+                </div>
+                </>
+                :
+                <>
+                <div className="nav-item">
+                    <a className="nav-link" href="/explore">About Us</a>
+                </div>
+                <div className="nav-item">
+                    <a className="nav-link" href="/store">Explore</a>
+                </div>
+                <div className="nav-item">
+                    <a className="nav-link" href="/profile">Register</a>
+                </div>
+                <div className="nav-item">
+                    <div className="logout-container" onClick={() => logOut()}>
+                        <FontAwesomeIcon icon={faDoorOpen}/>
+                    </div>
                 </div>
                 <div className='btn-login'>
                     <Login/>
                 </div>
+                </>
+            } 
             </ul>
         </div>
     )
 }
 
-export default Header;
+const mapStateToProps = (state) => {
+    return {
+        user: state.user,
+        token: state.token
+    }
+} 
+export default connect(mapStateToProps)(Header);
+
