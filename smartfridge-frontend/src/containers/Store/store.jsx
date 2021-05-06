6ff7e8aba924c36cfa46d234cd8dcaab70875738
Laura <lauraporto596@gmail.com>
2021-05-06 21:03:21 +0200

    grid with the results of search ingredients

--- smartfridge-frontend/src/containers/Store/store.jsx
+++ smartfridge-frontend/src/containers/Store/store.jsx
@@ -4,71 +4,69 @@ import axios from 'axios';
 
 import Search from '../../components/Search/search';
 import Header from '../../components/Header/header';
-import Ingredient from '../../components/Ingredient/ingredient';
+
 
 import kitchen from '../../assets/kitchen.jpeg';
 
 import { SAVE } from '../../redux/types/recipeType';
 import { connect } from 'react-redux';
 
+const apiKey = '?apiKey=d6e877dd55e74b919c1cf042e3e465bb';
+const query = '&ingredients=';
 
 const Store = (props) => {
 
     const history = useHistory();
-
-    // const [list, setList] = useState({
-    //     myIngredients: []
-    // });
-
-    // const selectIngredient = () => {
-
-    //     //Añadir al array list el elemento seleccionado mediante onClick
-    //     //setList(list); //
-
-    // }
- 
     const ingredients = props.myIngredients.myIngredients;
 
-    console.log(props.myIngredients.myIngredients, 'props');
 
+    //Hooks para hacer la lista de ingredientes
+    const [list, setList] = useState({
+        myIngredients: []
+    });
+ 
+    
     const getRecipes = async () => {
-        // const recipeData = await axios.get('https://api.spoonacular.com/recipes/findByIngredients?apiKey=d6e877dd55e74b919c1cf042e3e465bb&ingredients=cheese');
+        const recipeData = await axios.get(`https://api.spoonacular.com/recipes/findByIngredients${apiKey}${query}cheese`);
         //APIKEY y props.myIngredients[]
 
-        //setRecipeResult(recipeData.data);
-
-        // props.dispatch({type: SAVE, payload: recipeData.data});
-
-        // console.log(recipeData.data, 'reciperesult');
-        // console.log(recipeData.data[1], 'numero 1');
+        props.dispatch({type: SAVE, payload: recipeData.data});
 
         return setTimeout(() => {
             history.push('/result');
-        }, 300);
+        }, 500);
 
     };
 
     const selectIngredient = () => {
-        
+        // const listRecipe = list.myIngredients.push(ingredients.name);
+
+        // console.log(list.myIngredients, 'lista para las recetas');
+
+        // setList(list.myIngredients.push(ingredients.name));
+        // console.log(list.myIngredients);
     }
 
 
     return (
         <div className="main-store">
             <div className="header-div"> <Header/> </div>
-            <div className="search-store-container">
-                <Search/>
-            </div>
+           
             <div className="body-store">
                 <div className="store-fridge">
                     <img src={kitchen} style={{maxWidth: '100%', width: 'auto', height: '33em'}}></img>
                     <div className="fridge-menu">
                         <button>boton para borrrar</button>
                         <button>boton para añadir</button>
+                        <div className="search-store-container">
+                            <Search/>
+                        </div>
                     </div>
                 </div>
                 <div className="my-products-store">
-                    My Fridge
+                    <div className="selected-ingredients">
+                        Mis elementos seleccionados
+                    </div>
                     
                     <div className="ingredient-container">
                         {
@@ -98,4 +96,3 @@ const mapStateToProps = (state) => {
 }
 
 export default connect(mapStateToProps)(Store);
-// export default Store;
